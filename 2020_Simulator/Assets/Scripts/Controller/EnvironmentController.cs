bfcf2b7fc2c8af8ad515c2dcb569125f838766e8
LauraMarie <lauramarie8131@gmail.com>
2020-12-12 21:53:02 +0100

    GameOver countdown

---
+++ 2020_Simulator/Assets/Scripts/Controller/EnvironmentController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentController : MonoBehaviour
+{
+
+    public float timeRemaining = 1200;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        if(timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            LevelEnd(); 
+        }
+    }
+
+    public void LevelEnd()
+    {
+        UnityEngine.Debug.Log("Game Over");
+    }
+}
